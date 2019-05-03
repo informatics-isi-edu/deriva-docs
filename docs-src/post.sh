294e1832bfd54f420a34e83ca34e80e2fe17e573
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-03 16:36:01 -0700

    Almost there...

--- docs-src/post.sh
+++ docs-src/post.sh
@@ -6,7 +6,8 @@ rsync -avz -delete ./_build/html/ ../docs
 
 echo -e "\nCommit and push files to GitHub...\n"
 cd ../docs
-MESSAGE="Automated update: `date`"
+GITHUB_USER=${GITHUB_USER:-isi-isrd-dev}
+git config user.name ${GITHUB_USER} && git config user.email '{$GITHUB_USER}@users.noreply.github.com'
 git add -A .
-git commit -m "${MESSAGE}" .
+git commit -m "Automated update: `date`" .
 git push --dry-run

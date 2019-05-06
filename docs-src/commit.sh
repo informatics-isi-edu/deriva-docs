ea0538402476183840c4004bff00daaaf86dd0d1
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-06 16:08:07 -0700

    Fix path.

--- docs-src/commit.sh
+++ docs-src/commit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e -o pipefail
 
-echo -e "\nCommit and push files to GitHub...\n"
-cd ../docs
+echo -e "\nCommit and push generated doc files to GitHub...\n"
+cd ./docs
 GITHUB_USER=${GITHUB_USER:-isi-isrd-dev}
 GITHUB_EMAIL=${GITHUB_EMAIL:-isrddev@isi.edu}
 git config user.name ${GITHUB_USER} && git config user.email ${GITHUB_EMAIL}

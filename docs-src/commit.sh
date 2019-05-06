7a82d1a86fd60cfe323f2142bf767bd916136274
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-06 13:12:05 -0700

    Decouple commit/push from build process.

--- docs-src/commit.sh
+++ docs-src/commit.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e -o pipefail
 
-echo -e "\nCopying (rsync) files to docs directory...\n"
-rsync -avz -delete ./_build/html/ ../docs
-
 echo -e "\nCommit and push files to GitHub...\n"
 cd ../docs
 GITHUB_USER=${GITHUB_USER:-isi-isrd-dev}

0dcea1afb4e5d48c1dbfd2b7c39a4c672c8ab35c
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-06 22:00:00 -0700

    More tweaks.

--- commit.sh
+++ commit.sh
@@ -2,9 +2,6 @@
 set -e -o pipefail
 
 echo -e "\nCommit and push generated doc files to GitHub...\n"
-GITHUB_USER=${GITHUB_USER:-isi-isrd-dev}
-GITHUB_EMAIL=${GITHUB_EMAIL:-isrddev@isi.edu}
-git config user.name ${GITHUB_USER} && git config user.email ${GITHUB_EMAIL}
 git add -A ./docs
 git commit -m "Automated update: `date`" ./docs
 git push

121b76fc720a59977c7c21598f863f3b43ccf22b
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-06 17:21:25 -0700

    Relocate commit.sh. Tweak rsync.

--- commit.sh
+++ commit.sh
@@ -2,10 +2,9 @@
 set -e -o pipefail
 
 echo -e "\nCommit and push generated doc files to GitHub...\n"
-cd ./docs
 GITHUB_USER=${GITHUB_USER:-isi-isrd-dev}
 GITHUB_EMAIL=${GITHUB_EMAIL:-isrddev@isi.edu}
 git config user.name ${GITHUB_USER} && git config user.email ${GITHUB_EMAIL}
-git add -A .
-git commit -m "Automated update: `date`" .
+git add -A ./docs
+git commit -m "Automated update: `date`" ./docs
 git push

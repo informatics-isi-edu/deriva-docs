0e9e72ecc63a6b0211301f6f095d8e677db8c50c
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-23 19:01:52 -0700

    Try to get rid of installation dependency for autodocs by searching path per sphinx docs.

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -12,9 +12,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath('../repo-src/deriva-py'))
+sys.path.append(os.path.abspath('../repo-src/deriva-catalog-manage'))
 
 # -- Project information -----------------------------------------------------
 

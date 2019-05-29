9b4a866a2e2c2860986d999ceca003f675ec56d4
mikedarcy <mikedarcy@users.noreply.github.com>
2019-05-28 17:03:00 -0700

    Set release tag to "current".

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -29,7 +29,7 @@ html_baseurl = 'http://docs.derivacloud.org'
 # The short X.Y version
 #version = '1.0'
 # The full version, including alpha/beta/rc tags
-#release = '1.0.0-alpha'
+release = 'current'
 
 
 # -- General configuration ---------------------------------------------------

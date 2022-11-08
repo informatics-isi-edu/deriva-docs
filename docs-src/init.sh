774f354fea866b975c89862a1a6cf666aa1061c8
mikedarcy <mdarcy@isi.edu>
2022-11-08 14:25:12 -0800

    Add deriva-workbench to docs build.

--- docs-src/init.sh
+++ docs-src/init.sh
@@ -7,7 +7,7 @@ echo -e "Removing existing source repos...\n"
 rm -rf ../repo-src
 
 DEFAULT_TAG=master
-REPO_TAGS=( DERIVAPY_TAG DERIVAQT_TAG DERIVAWEB_TAG ERMREST_TAG HATRAC_TAG ERMRESTJS_TAG CHAISE_TAG DERIVA_CATALOG_MANAGE_TAG )
+REPO_TAGS=( DERIVAPY_TAG DERIVAQT_TAG DERIVAWEB_TAG ERMREST_TAG HATRAC_TAG ERMRESTJS_TAG CHAISE_TAG DERIVA_CATALOG_MANAGE_TAG DERIVA_WORKBENCH_TAG)
 echo -e "Cloning the following repositories:\n"
 for TAG in ${REPO_TAGS[@]}; do
   export ${TAG}=${!TAG:-${DEFAULT_TAG}}
@@ -27,12 +27,14 @@ git clone -b ${HATRAC_TAG} --depth 1 https://${GITHUB_AUTH}github.com/informatic
 git clone -b ${ERMRESTJS_TAG} --depth 1 https://${GITHUB_AUTH}github.com/informatics-isi-edu/ermrestjs ../repo-src/ermrestjs
 git clone -b ${CHAISE_TAG} --depth 1 https://${GITHUB_AUTH}github.com/informatics-isi-edu/chaise ../repo-src/chaise
 git clone -b ${DERIVA_CATALOG_MANAGE_TAG} --depth 1 https://${GITHUB_AUTH}github.com/informatics-isi-edu/deriva-catalog-manage ../repo-src/deriva-catalog-manage
+git clone -b ${DERIVA_WORKBENCH_TAG} --depth 1 https://${GITHUB_AUTH}github.com/informatics-isi-edu/deriva-workbench ../repo-src/deriva-workbench
 
 echo -e "\nInstall code dependencies:\n"
 # temp hack for setuptools_scm
 pip install --no-warn-script-location --user "setuptools_scm<6.0"
 pip install --no-warn-script-location --user ../repo-src/deriva-py
 pip install --no-warn-script-location --user ../repo-src/deriva-catalog-manage
+pip install --no-warn-script-location --user ../repo-src/deriva-workbench
 
 echo -e "\nSymlink files and directories...\n"
 rm -f ./deriva-py && ln -sv ../repo-src/deriva-py/docs/ ./deriva-py
@@ -43,6 +45,7 @@ rm -f ./hatrac && ln -sv ../repo-src/hatrac/docs/ ./hatrac
 rm -f ./ermrestjs && ln -sv ../repo-src/ermrestjs/docs/ ./ermrestjs
 rm -f ./chaise && ln -sv ../repo-src/chaise/docs/ ./chaise
 rm -f ./deriva-catalog-manage && ln -sv ../repo-src/deriva-catalog-manage/docs/ ./deriva-catalog-manage
+rm -f ./deriva-workbench && ln -sv ../repo-src/deriva-workbench/docs/ ./deriva-workbench
 
 cd ./users-guide/
 ln -sfv ../deriva-py/project-tutorial.md
